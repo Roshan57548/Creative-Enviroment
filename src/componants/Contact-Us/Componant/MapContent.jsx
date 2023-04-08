b6a745b94d9f37466e6ed63456afda17b64e1589
roshan.prajapati <roshanprajapati678@gmail.com>
2023-04-08 14:41:24 +0530

    add pages

---
+++ src/componants/Contact-Us/Componant/MapContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Style/MapContent.css';
+const MapContent = () => {
+  return (
+    <div>MapContent</div>
+  )
+}
+
+export default MapContent
---
+++ src/componants/Services/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Style/Services.css';
+const Services = () => {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
