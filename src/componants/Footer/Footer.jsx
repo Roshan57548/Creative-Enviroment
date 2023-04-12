bcf8f874a22ae35e1a2a125a3862549085566e6d
vratant11 <vratant2012061@akgec.ac.in>
2023-04-12 23:01:34 +0530

    Header and footer structure ready

--- src/componants/Footer/Footer.jsx
+++ src/componants/Footer/Footer.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
-import './Style/Footer.css';
+import React from "react";
+import "./Style/Footer.css";
+import HomeIcon from "@mui/icons-material/Home";
+import CallIcon from "@mui/icons-material/Call";
+import EmailIcon from "@mui/icons-material/Email";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <footer>
+        <div class="row primary">
+          <div class="column about">
+            <h3>ENVYSIS</h3>
 
-export default Footer
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
+              nihil nisi quo quisquam veniam cumque ex officia neque, quidem
+              labore laborum! Cum vel facilis fugiat culpa beatae dicta
+              obcaecati exercitationem ab eaque ipsam qui incidunt repellat
+              iusto aliquid possimus fuga dolores, animi nam ex ipsa
+              consectetur! Nesciunt molestias sunt reprehenderit.
+            </p>
+
+            <div class="social">
+              <i class="fa-brands fa-facebook-square"></i>
+              <i class="fa-brands fa-instagram-square"></i>
+              <i class="fa-brands fa-twitter-square"></i>
+              <i class="fa-brands fa-youtube-square"></i>
+              <i class="fa-brands fa-whatsapp-square"></i>
+            </div>
+          </div>
+
+          <div class="column links">
+            <h3>Some Links</h3>
+
+            <ul>
+              <li>
+                <a href="#faq">Home</a>
+              </li>
+              <li>
+                <a href="#cookies-policy">About</a>
+              </li>
+              <li>
+                <a href="#terms-of-services">Products</a>
+              </li>
+              <li>
+                <a href="#support">Services</a>
+              </li>
+              <li>
+                <a href="#support">Testing Services</a>
+              </li>
+              <li>
+                <a href="#support">Blog</a>
+              </li>
+              <li>
+                <a href="#support">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="column links">
+            <h3>Premium Poducts</h3>
+            <ul>
+              <li>
+                <a href="#faq">Environmental Test </a>
+              </li>
+              <li>
+                <a href="#cookies-policy">Dust Chamber</a>
+              </li>
+              <li>
+                <a href="#terms-of-services">Thermal Stock Chamber</a>
+              </li>
+              <li>
+                <a href="#support">Vibration Chamber</a>
+              </li>
+              <li>
+                <a href="#support">Salt Spray Chamber</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="column links">
+            <h3>Contact-Info</h3>
+            <ul>
+              <li>
+                <HomeIcon />
+                <a href="#faq">366 Fawn St. Oak Creek, WI 53154</a>
+              </li>
+              <li>
+                <CallIcon />
+                <a href="#cookies-policy">+91 8707321772 +91 87654321289</a>
+              </li>
+              <li>
+                <EmailIcon />
+                <a href="#terms-of-services">Vratantsingh@123gmail.com</a>
+              </li>
+              <li>
+                <a href="#support">Support</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/componants/Header/Header.jsx
+++ src/componants/Header/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react';
+import Company_logo from '../../../src/Image/Company-Logo/Company_logo.png'
 import './Style/Header.css';
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+    
+    return (
+    <>
+    <div className="topnav" id="myTopnav">
+  <a href="#home" >Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <div className="dropdown">
+    <button className="dropbtn">Dropdown
+      <i className="fa fa-caret-down"></i>
+    </button>
+    <div className="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" className="icon" onClick={myFunction}>&#9776;</a>
+</div>
+    </>
+    )
 }
-
 export default Header
