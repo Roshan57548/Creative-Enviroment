50bfcd9879be8ddd2995771942f0fe62b28e08e9
ayushkumar32539 <ayush2010128@akgec.ac.in>
2023-04-12 21:54:06 +0530

    Carousel aboutus and testchamber

--- src/componants/Home/Componant/TestChamber.jsx
+++ src/componants/Home/Componant/TestChamber.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './Style/TestChamber.css';
-const TestChamber = () => {
+import tc_image from '../../../assets/tc_image.png'
+const TestChamber = (props) => {
   return (
-    <div>TestChamber</div>
+    <div className='tc_container'>
+    <div className="tc_wrapper">
+      <div className="tc_left">
+        {props.data.Image}
+      </div>
+      <div className="tc_right">
+        <div className="tc_textarea">
+          <h3>{props.data.heading}</h3>
+          <h1>{props.data.sub_heading}</h1>
+          <p>{props.data.para}</p>
+        </div>
+        <div className="tc_links">
+          <div className="tc_links_img">
+            {props.data.cards.cardimage}
+          </div>
+          <div className="tc_links_heading"><h3 className='tc_lk_heading'>Award for manufacturing excellence</h3></div>
+          <div className="tc_links_desp"><p className='tc_lc_desc'>award winner for excellence in manufacturing products</p></div>
+        </div>
+      </div>
+    </div>
+
+    </div>
   )
 }
 
