b51b3862625c87650764db35c6dcf25ff499dfcd
jtr-code <jtrtricks@gmail.com>
2023-04-16 21:54:46 +0530

    updates in about

--- src/componants/About/About.jsx
+++ src/componants/About/About.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import './Style/About.css';
-import Banner from './Componant/Banner';
-import CompanyDetails from './Componant/CompanyDetails';
-import OurVision from './Componant/OurVision';
-import MissionValue from './Componant/MissionValue';
-import OurFounders from './Componant/OurFounders';
-import StateOfCity from './Componant/StateOfCity';
-import CarryBig from './Componant/CarryBig';
-import Banners from './Content/Banners';
-import CompanyDetail from './Content/CompanyDetail';
-import OurVisions from './Content/OurVisions';
-import MissionValues from './Content/MissionValues';
+import React from "react";
+import "./Style/About.css";
+import Banner from "./Componant/Banner";
+import CompanyDetails from "./Componant/CompanyDetails";
+import OurVision from "./Componant/OurVision";
+// import MissionValue from "./Componant/MissionValue";
+// import OurFounders from "./Componant/OurFounders";
+// import StateOfCity from "./Componant/StateOfCity";
+import CarryBig from "./Componant/CarryBig";
+import Banners from "./Content/Banners";
+import CompanyDetail from "./Content/CompanyDetail";
+import OurVisions from "./Content/OurVisions";
+// import MissionValues from "./Content/MissionValues";
+
 const About = () => {
   return (
     <div>
-      <Banner data={Banners}/>
-      <CompanyDetails data={CompanyDetail}/>
-      <OurVision data={OurVisions}/> 
-       <MissionValue data={MissionValues}/>
-        {/* <OurFounders /> */}
-       {/* <StateOfCity />  */}
-       {/* <CarryBig /> */} 
+      <Banner data={Banners} />
+      <CompanyDetails data={CompanyDetail} />
+      <OurVision data={OurVisions} />
+      {/* <MissionValue data={MissionValues}/> */}
+      {/* <OurFounders /> */}
+      {/* <StateOfCity />  */}
+      {/* <CarryBig /> */}
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
