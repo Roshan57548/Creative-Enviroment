b05cd12e2744556a6d1da7608ff562e679778f23
jtr-code <jtrtricks@gmail.com>
2023-04-16 21:47:37 +0530

    ourvision updated

--- src/componants/About/Content/OurVisions.jsx
+++ src/componants/About/Content/OurVisions.jsx
@@ -1,12 +1,17 @@
 const OurVisions = {
-    Heading: "Hello",
-    Links: [
-        {
-            msg1:"To be a client-oriented company, the most preferred brand, and the supplier of choice, in every industry we serve.",
-            msg2:"Our ultimate desire is to enable our clients to achieve their objectives by manufacturing and supplying reliable and safe products now and in the future.",
-            msg3:"We have a team of professionals with extensive knowledge and experience who often go the extra mile to provide clients with the best experience.",
-        }
-    ]
+  Heading: "Our Vision",
+  Description: [
+    {
+      message1:
+        "Company Mission Statement: Our company's mission is to be the epitome of client orientation, setting the standard for excellence in every industry we serve. We strive to become the most preferred brand and supplier of choice, consistently delivering exceptional value and unparalleled service to our valued clients.",
+      message2:
+        "Core Values: At our company, we hold the following core values close to our heart as we work towards our goal of being client-oriented, preferred, and trusted:",
+      message3:
+        "Customer Focus: Our clients are at the heart of everything we do. We listen to their needs, anticipate their expectations, and go the extra mile to exceed them, building long-lasting relationships based on trust and mutual success.",
+      message4:
+        "Collaboration: We foster a culture of collaboration and teamwork, both internally and with our clients. We believe that by working together, we can achieve greater results and provide exceptional value to our clients.",
+    },
+  ],
 };
 
 export default OurVisions;
