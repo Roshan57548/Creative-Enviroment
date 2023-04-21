97dc78011494da8beacdbc85f49df73e581163c7
ayushkumar32539 <ayush2010128@akgec.ac.in>
2023-04-21 22:40:41 +0530

    our mission and our values

--- src/componants/About/About.jsx
+++ src/componants/About/About.jsx
@@ -3,14 +3,14 @@ import "./Style/About.css";
 import Banner from "./Componant/Banner";
 import CompanyDetails from "./Componant/CompanyDetails";
 import OurVision from "./Componant/OurVision";
-// import MissionValue from "./Componant/MissionValue";
+import MissionValue from "./Componant/MissionValue";
 // import OurFounders from "./Componant/OurFounders";
 // import StateOfCity from "./Componant/StateOfCity";
 import CarryBig from "./Componant/CarryBig";
 import Banners from "./Content/Banners";
 import CompanyDetail from "./Content/CompanyDetail";
 import OurVisions from "./Content/OurVisions";
-// import MissionValues from "./Content/MissionValues";
+import MissionValues from "./Content/MissionValues";
 
 const About = () => {
   return (
@@ -18,7 +18,7 @@ const About = () => {
       <Banner data={Banners} />
       <CompanyDetails data={CompanyDetail} />
       <OurVision data={OurVisions} />
-      {/* <MissionValue data={MissionValues}/> */}
+      <MissionValue data={MissionValues}/>
       {/* <OurFounders /> */}
       {/* <StateOfCity />  */}
       {/* <CarryBig /> */}
