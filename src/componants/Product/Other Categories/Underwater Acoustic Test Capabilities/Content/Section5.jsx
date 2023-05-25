59a74ecfea82cc46ae2e7239d94864f093e37d8e
roshan.prajapati <roshanprajapati678@gmail.com>
2023-05-25 23:44:55 +0530

    done

--- src/componants/Product/Other Categories/Underwater Acoustic Test Capabilities/Content/Section5.jsx
+++ src/componants/Product/Other Categories/Underwater Acoustic Test Capabilities/Content/Section5.jsx
@@ -1,10 +1,11 @@
+import image1 from '../Component/Images/simple.jpg';
 const Sections5 = {
-    type : 'SignalSound UW-350 Low Frequency Projector',
+    type : 'SignalSound CUW-350 Low Frequency Projector',
     heading : 'Technical Specs',
     shead : '20 Hz to 20 kHz',
     sub: 'Amplifier drive: 1 kVA',
     link: 'Weight: 100 kg',
-    src: 'https://www.dataphysics.com/wp-content/uploads/2021/07/SignalSound-UW350-underwater-acoustic-projector-Data-Physics-2-1011x1024.jpg',
+    src: image1,
 }
 
 export default Sections5;
