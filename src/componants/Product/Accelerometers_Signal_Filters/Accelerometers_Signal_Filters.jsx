c7ac14782e974aa3c332f596d977eca97f04bfe4
sakshya25 <2573st@gmail.com>
2023-06-15 19:44:14 +0530

    updation Section1

--- src/componants/Product/Accelerometers_Signal_Filters/Accelerometers_Signal_Filters.jsx
+++ src/componants/Product/Accelerometers_Signal_Filters/Accelerometers_Signal_Filters.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Style/Accelerometers_Signal_Filters.css";
+import Section1 from "./Component/Section1";
 const Accelerometers_Signal_Filters = () => {
   return (
     <div className="Accelerometers_Signal_Filters">
-      Accelerometers_Signal_Filters
+      <Section1/>
     </div>
   );
 };
