e68629e4f5a9869407315ef71ae2e4fc81c8f038
ayushkumar32539 <ayush2010128@akgec.ac.in>
2023-06-16 00:16:52 +0530

    Sectin 2,3,4

--- src/componants/Product/Accelerometers_Signal_Filters/Accelerometers_Signal_Filters.jsx
+++ src/componants/Product/Accelerometers_Signal_Filters/Accelerometers_Signal_Filters.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import "./Style/Accelerometers_Signal_Filters.css";
 import Section1 from "./Component/Section1";
+import Section2 from "./Component/Section2";
+import Sections2 from "./Content/Section2";
+import Section3 from "./Component/Section3";
+import Sections3 from "./Content/Section3";
+import Section4 from "./Component/section4";
 const Accelerometers_Signal_Filters = () => {
   return (
     <div className="Accelerometers_Signal_Filters">
       <Section1/>
+      <Section2 data={Sections2}/>
+      <Section3 data={Sections3}/>
+      <Section4/>
     </div>
   );
 };
--- src/componants/Product/Accelerometers_Signal_Filters/Component/Section1.jsx
+++ src/componants/Product/Accelerometers_Signal_Filters/Component/Section1.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import "./Style/Section1.css"
 import { useNavigate } from "react-router-dom";
