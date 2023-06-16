41636e1812c1a42db8d82da2d4b0fa7aa5d6d5bc
kuldeep <kuldeepkumar58426@gmail.com>
2023-06-16 11:07:07 +0530

    Added Section-5,6,7 in Acc_Signal_fil product

--- src/componants/Product/Accelerometers_Signal_Filters/Accelerometers_Signal_Filters.jsx
+++ src/componants/Product/Accelerometers_Signal_Filters/Accelerometers_Signal_Filters.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Style/Accelerometers_Signal_Filters.css";
 import Section1 from "./Component/Section1";
+import Section5 from "./Component/Section5";
+import Section6 from "./Component/Section6";
+import Section7 from "./Component/Section7";
+import ContentSection5 from "./Component/Content/ContentSection5";
+import ContentSection6 from "./Component/Content/contentSection6";
+import ContentSection7 from "./Component/Content/contentSection7";
 const Accelerometers_Signal_Filters = () => {
   return (
     <div className="Accelerometers_Signal_Filters">
       <Section1/>
+      <Section5 data={ContentSection5}/>
+      <Section6 data={ContentSection6}/>
+      <Section7 data={ContentSection7}/>
     </div>
   );
 };
